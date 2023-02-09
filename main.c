b72176b1d902be25f79e25cd087449632fcf3513
quentinlfv <qlefevre@e2r4p13.clusters.42paris.fr>
2023-02-09 18:30:22 +0100

    push

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int check_args(int argc)
 {
     if (argc > 5)
     {
-        perror("too much args");
+        perror("too much args pipis");
         return (0);
     }
     printf("number of args : OK\n");
@@ -13,100 +13,134 @@ int check_args(int argc)
 
 void *routine(void *arg)
 {
-    t_philo p;
+    t_indiv p;
 
-    p = *(t_philo *)arg;
-    gettimeofday(&p.action, NULL);
-    printf("%ld    Hello im a philosopher number\n", ((p.action.tv_sec * 1000 + p.action.tv_usec) -
-    (p.start.tv_sec * 1000 + p.start.tv_usec)));
-    return (NULL);
-}
-
-void *watch(void *arg)
-{
-    struct timeval cmp;
-    size_t time;
-    size_t i;
-    t_philo p;
-
-    p = *(t_philo *)arg;
-    while (1)
-    {
-        i = 0;
-        while (i < p.nu_philo)
-        {
-            printf("yo");
-            gettimeofday(&cmp, NULL);
-            time = cmp.tv_usec; 
-            if (time >= p.my_philo[i].die_in)
-            {
-                printf("%ld died\n", i);
-                // free
-                exit(1);
-            }
-            i++;
-        }
-    }
+    p = *(t_indiv *)arg;
+    usleep(p.ptr->time_to_eat * 1000);
+    usleep(p.ptr->time_to_sleep * 1000);
+    /* thinking */
     // gettimeofday(&p.action, NULL);
-    // printf(" %ld    Hello im a philosopher number\n", ((p.action.tv_sec * 1000 + p.action.tv_usec) -
-    // (p.start.tv_sec * 1000 + p.start.tv_usec)));
+    // printf("%ld    Hello im a philosopher number and my id is %d\n", ((p.action.tv_sec * 1000 + p.action.tv_usec) -
+    // (p.start.tv_sec * 1000 + p.start.tv_usec)), p.my_philo);
     return (NULL);
 }
 
-t_philo    init_var(t_philo p, char **argv)
+
+void    msg(t_indiv p)
 {
-    p.nu_philo = ft_atoi(argv[1]);
-    p.time_to_die = ft_atoi(argv[2]);
-    p.time_to_eat = ft_atoi(argv[3]);
-    p.time_to_sleep = ft_atoi(argv[4]);
+    struct timeval action;
+    gettimeofday(&action, NULL);
+    printf("%ld    Hello im a philosopher number and my id is %d\n", ((action.tv_sec * 1000 + action.tv_usec) -
+    (p.ptr->start.tv_sec * 1000 + p.ptr->start.tv_usec)), p.id);
+}
+
+// void    reset_dead_timer(t_philo p, int id)
+// {
+//     size_t  time;
+
+//     gettimeofday(&p.my_philo[id].get_time, NULL);
+//     p.my_philo[id] = ((p.my_philo[id].get_time.tv_sec * 1000 + p.my_philo[id].get_time.tv_usec) -
+//                     (p.start.tv_sec * 1000 + p.start.tv_usec));
+    
+
+// }
+
+// void *watch(void *arg)
+// {
+//     struct timeval cmp;
+//     size_t time;
+//     size_t i;
+//     t_philo p;
+
+//     p = *(t_philo *)arg;
+//     printf("die in watch : %ld\n", p.my_philo[0].dead_timer);
+//     while (1)
+//     {
+//         i = 0;
+//         while (i < p.nu_philo)
+//         {
+//             gettimeofday(&cmp, NULL);
+//             time = ((cmp.tv_sec * 1000 + cmp.tv_usec) -
+//                     (p.start.tv_sec * 1000 + p.start.tv_usec));
+//             printf("timer : %ld\n", time); 
+//             if (time >= p.my_philo[i].dead_timer)
+//             {
+//                 printf("%ld died\n", i);
+//                 // free
+//                return (NULL);
+//                 // exit(1);
+//             }
+//             usleep(50);
+//             i++;
+//         }
+//     }
+//     return (NULL);
+// }
+
+t_philo    init_var(t_philo data, char **argv)
+{
+    data.nu_philo = ft_atoi(argv[1]);
+    data.time_to_die = ft_atoi(argv[2]);
+    data.time_to_eat = ft_atoi(argv[3]);
+    data.time_to_sleep = ft_atoi(argv[4]);
+    return (data);
+}
+
+void    init_death_timer(t_indiv *p)
+{
+    size_t  i;
+    size_t  nu;
+
+    i = 0;
+    nu = p[i].ptr->nu_philo;
+    while (i < nu)
+    {
+        p[i].death_timer = p[i].ptr->time_to_die;
+        i++;
+    }
+}
+
+t_indiv    *init_philo(t_indiv *p, t_philo *data)
+{
+    size_t i;
+
+    p = malloc(sizeof(t_indiv) * data->nu_philo);
+    if (!p)
+    {
+        perror("Failed to malloc\n");
+        return (NULL);
+    }
+    i = 0;
+    while (i < data->nu_philo)
+    {
+        p[i].id = i;
+        p[i].ptr = data;
+        i++;
+    }
+    printf("nu phil : %ld\n", p[0].ptr->nu_philo);
     return (p);
 }
 
-int init_die_in(t_philo p)
+
+/* ---------------------------------------------------------------------*/
+/*  INIT PHILO THREAD */
+int init_thread(t_indiv *p, size_t nu)
 {
-    size_t i;
+    size_t  i;
 
-    p.my_philo = malloc(sizeof(t_indiv) * p.nu_philo);
-    printf("start in ms : %ld\n", p.start.tv_usec);
-    if(!p.my_philo)
-    {
-        perror("failed to malloc\n");
-        return (0);
-    }
     i = 0;
-    while (i < p.nu_philo)
+    while (i < nu)
     {
-        p.my_philo[i].die_in =  p.start.tv_usec + p.time_to_die * 1000;
-        printf("die in for philo nu %ld : %ld\n", i, p.my_philo[i].die_in);
-        i++;
-    }
-    return (1);
-}
-
-int    init_philo(t_philo p)
-{
-    size_t i;
-
-    p.philo = malloc(sizeof(pthread_t) * p.nu_philo);
-    if (!p.philo)
-    {
-        perror("Failed to malloc\n");
-        return (0);
-    }
-    i = 0;
-    while (i < p.nu_philo)
-    {
-        if (pthread_create(&p.philo[i], NULL, &routine, &p) != 0)
+        if (pthread_create(&p[i].philo, NULL, &routine, &p[i]) != 0)
         {
             perror("Failed to create thread\n");
             return (0);
         }
-        i++;
     }
     i = 0;
-    while (i < p.nu_philo)
+    while (i < nu)
     {
-          if (pthread_join(p.philo[i], NULL) != 0)
+          if (pthread_join(p[i].philo, NULL) != 0)
           {
             perror("Failed to join");
             return (0);
@@ -115,37 +149,44 @@ int    init_philo(t_philo p)
     }
     return (1);
 }
+/* --------------------------------------------------------------------*/
 
-int create_watch(t_philo p)
-{
-    if (pthread_create(&p.watch, NULL, &watch, &p) != 0)
-    {
-        perror("Failed to create thread\n");
-        return (0);
-    }
-    if (pthread_join(p.watch, NULL) != 0)
-    {
-        perror("Failed to join\n");
-        return (0);
-    }
-    return (1);
-}
+
+// int create_watch(t_philo p)
+// {
+//     if (pthread_create(&p.watch, NULL, &watch, &p) != 0)
+//     {
+//         perror("Failed to create thread\n");
+//         return (0);
+//     }
+//     if (pthread_join(p.watch, NULL) != 0)
+//     {
+//         perror("Failed to join\n");
+//         return (0);
+//     }
+//     return (1);
+// }
 
 int main(int argc, char **argv)
 {
-    t_philo p;
+    t_indiv *p;
+    t_philo data;
+    // struct timeval start;
 
-    gettimeofday(&p.start, NULL);
     if (!check_args(argc))
         return (2);
-    p = init_var(p, argv);
-    p.philo = NULL;
-    if (!init_philo(p))
+    gettimeofday(&data.start, NULL);
+    data = init_var(data, argv);
+    p = NULL;
+    p = init_philo(p, &data);
+    if (p == NULL)
         return (2);
-    if (!init_die_in(p))
+    init_death_timer(p);
+    printf("death_timer : %ld\n", p[0].death_timer);
+    if(!init_thread(p, p[0].ptr->nu_philo))
         return (2);
-    if (!create_watch(p))
-        return (2);
+    // if (!create_watch(p))
+    //     return (2);
     // if (!init_mutex(p))
     //     return (2);
     // destroy_mutex(p);
--- philo.h
+++ philo.h
@@ -7,38 +7,41 @@
 # include <pthread.h>
 # include <sys/time.h>
 
-typedef struct s_indiv
-{
-    size_t die_in;
-}t_indiv;
 
 typedef struct s_philo
 {
-    t_indiv *my_philo;
     size_t nu_philo;
     size_t time_to_die;
     size_t time_to_eat;
     size_t time_to_sleep;
     struct timeval start;
-    struct timeval action;
-    pthread_t *philo;
-    pthread_t watch;
-    pthread_mutex_t *mutex;
+    // struct timeval action;
+    // pthread_t watch;
+    // pthread_mutex_t *mutex;
 }t_philo;
 
+typedef struct s_indiv
+{
+    int         id;
+    size_t      death_timer;
+    pthread_t   philo;
+    // struct timeval get_time;
+    t_philo *ptr;
+}t_indiv;
+
 /* main.c */
-int     check_args(int argc);
-void    *routine(void *arg);
-void    *watch(void *arg);
-int     create_watch(t_philo p);
-t_philo    init_var(t_philo p, char **argv);
-int     init_philo(t_philo p);
-int     init_die_in(t_philo p);
+int         check_args(int argc);
+void        *routine(void *arg);
+void        *watch(void *arg);
+int         create_watch(t_philo p);
+t_philo     init_var(t_philo p, char **argv);
+t_indiv     *init_philo(t_indiv *p, t_philo *data);
+void        init_death_timer(t_indiv *p);
+int init_thread(t_indiv *p, size_t nu);
 
 /* mutex.c */
-int    init_mutex(t_philo p);
+int     init_mutex(t_philo p);
 void    destroy_mutex(t_philo p);
-
 
 /* functions.c */
 size_t	ft_atoi(const char *nptr);
